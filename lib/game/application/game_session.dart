ca5f4df3e6246d46aaee600ba10faa650bf123bd
Julio Rivera <julio.fincazero@proton.me>
2026-07-08 20:06:28 -0400

    Autosave pet session changes

--- lib/game/application/game_session.dart
+++ lib/game/application/game_session.dart
@@ -1,3 +1,4 @@
+import '../data/pet_repository.dart';
 import '../domain/entities/pet.dart';
 import '../domain/rules/pet_action_type.dart';
 import '../domain/services/game_engine.dart';
@@ -8,20 +9,29 @@ class GameSession {
 
   final GameEngine gameEngine;
   final TimeEngine timeEngine;
+  final PetRepository petRepository;
 
   GameSession({
     required this.pet,
     GameEngine? gameEngine,
     TimeEngine? timeEngine,
+    PetRepository? petRepository,
   })  : gameEngine = gameEngine ?? GameEngine(),
-        timeEngine = timeEngine ?? TimeEngine.standard();
+        timeEngine = timeEngine ?? TimeEngine.standard(),
+        petRepository = petRepository ?? PetRepository();
 
-  void performAction(PetActionType action) {
+  Future<void> performAction(PetActionType action) async {
     pet = gameEngine.applyAction(pet, action);
+    await petRepository.savePet(pet);
   }
 
-  void advanceMinutes(int minutes) {
+  Future<void> advanceMinutes(int minutes) async {
     final futureTime = pet.lastUpdatedAt.add(Duration(minutes: minutes));
     pet = timeEngine.progress(pet, futureTime);
+    await petRepository.savePet(pet);
+  }
+
+  Future<void> save() async {
+    await petRepository.savePet(pet);
   }
 }
